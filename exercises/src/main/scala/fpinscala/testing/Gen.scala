2ae0003e57905d5598078f481072613c17a29144
Vincent Theron <vptheron@gmail.com>
2015-05-14 07:47:21 -0400

    start property based testing

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -14,20 +14,57 @@ shell, which you can fill in and modify while working through the chapter.
 */
 
 trait Prop {
+
+  def check: Either[(FailedCase,SuccessCount), SuccessCount]
+
+//  def &&(p: Prop): Prop = new Prop {
+//    def check: Boolean = this.check && p.check
+//  }
+
 }
 
 object Prop {
+  type SuccessCount = Int
+  type FailedCase = String
+
   def forAll[A](gen: Gen[A])(f: A => Boolean): Prop = ???
 }
 
 object Gen {
-  def unit[A](a: => A): Gen[A] = ???
+  def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
+
+  val boolGen: Gen[Boolean] = Gen(
+    State(_.nextInt match {
+      case (n,rng) => (n%2 == 0,rng)
+    })
+  )
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = Gen(
+    State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive-start))
+  )
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State.sequence(List.fill(n)(g.sample)))
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    boolGen.flatMap(b => if(b) g1 else g2)
+
 }
 
-trait Gen[A] {
-  def map[A,B](f: A => B): Gen[B] = ???
-  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+case class Gen[A](sample: State[RNG,A]) {
+
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
+    Gen(sample.flatMap(a => f(a).sample))
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] =
+    size.flatMap(Gen.listOfN(_,this))
+
 }
+
+//trait Gen[A] {
+//  def map[A,B](f: A => B): Gen[B] = ???
+//  def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
+//}
 
 trait SGen[+A] {
 
